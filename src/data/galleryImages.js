a80627ba17736610078de110b149da05396cf56d
LuygiZ <lmname999@gmail.com>
2025-05-13 23:10:53 +0100

    yes!

--- src/data/galleryImages.js
+++ src/data/galleryImages.js
@@ -1,10 +1,17 @@
+import img1 from '../assets/trabalhos/1.png';
+import img2 from '../assets/trabalhos/2.png';
+import img3 from '../assets/trabalhos/3.png';
+import img4 from '../assets/trabalhos/4.png';
+import img5 from '../assets/trabalhos/5.png';
+import img6 from '../assets/trabalhos/6.png';
+
 const galleryImages = [
-  "./src/assets/trabalhos/1.png",
-  "./src/assets/trabalhos/2.png",
-  "./src/assets/trabalhos/3.png",
-  "./src/assets/trabalhos/4.png",
-  "./src/assets/trabalhos/5.png",
-  "./src/assets/trabalhos/6.png",
+  img1,
+  img2,
+  img3,
+  img4,
+  img5,
+  img6,
 ];
 
 export default galleryImages;
