75c62db09dd04be7421880c6af75b89e07ddb260
LuygiZ <lmname999@gmail.com>
2025-05-13 23:08:33 +0100

    Imports

--- src/data/service.js
+++ src/data/service.js
@@ -1,39 +1,46 @@
+import sobrancelhaPersonalizada from '../assets/servicos/sobrancelha_personalizada.png';
+import sobrancelhaHenna from '../assets/servicos/sobrancelha_henna.png';
+import tinturaSobrancelha from '../assets/servicos/tintura_sobrancelha.png';
+import browLamination from '../assets/servicos/brow_lamination.png';
+import lashLifting from '../assets/servicos/lash_lifting.png';
+import extensionPestanas from '../assets/servicos/extension_pestanas.png';
+
 const services = [
   {
     id: 1,
     name: "Design de Sobrancelhas Personalizado",
     description: "Realçamos a beleza natural do seu rosto com um design de sobrancelhas feito à medida, respeitando a simetria e os traços únicos de cada cliente.",
-    image: "./src/assets/servicos/sobrancelha_personalizada.png"
+    image: sobrancelhaPersonalizada
   },
   {
     id: 2,
     name: "Design de Sobrancelhas com Henna",
     description: "Aposte em sobrancelhas definidas e duradouras com henna, ideal para quem deseja um efeito de preenchimento natural sem dor ou compromisso permanente.",
-    image: "./src/assets/servicos/sobrancelha_henna.png"
+    image: sobrancelhaHenna
   },
   {
     id: 3,
     name: "Tintura de Sobrancelhas e Pestanas",
     description: "Destaque o seu olhar com a tintura de sobrancelhas e pestanas, perfeita para intensificar a cor e dispensar o uso diário de maquilhagem.",
-    image: "./src/assets/servicos/tintura_sobrancelha.png"
+    image: tinturaSobrancelha
   },
   {
     id: 4,
     name: "Brow Lamination",
     description: "Alcance sobrancelhas mais volumosas, alinhadas e com um acabamento sofisticado através do Brow Lamination – tendência que transforma o seu olhar.",
-    image: "./src/assets/brow_lamination.png"
+    image: browLamination
   },
   {
     id: 5,
     name: "Lash Lifting",
     description: "Eleve e curve naturalmente as suas pestanas com o Lash Lifting – uma alternativa perfeita às extensões, com resultados duradouros e sem manutenção diária.",
-    image: "./src/assets/servicos/lash_lifting.png"
+    image: lashLifting
   },
   {
     id: 6,
     name: "Extensão de Pestanas",
     description: "Obtenha pestanas longas, cheias e impactantes com a aplicação profissional de extensões – ideal para realçar o olhar com um toque glamoroso e elegante.",
-    image: "./src/assets/servicos/extension_pestanas.png"
+    image: extensionPestanas
   }
 ];
 
